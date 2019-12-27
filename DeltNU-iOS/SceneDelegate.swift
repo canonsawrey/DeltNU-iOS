a8d2e408790bddf457035ebc021f5f357d4019f8
Canon Sawrey <csawrey@rightpoint.com>
2019-12-27 00:52:12 -0500

    Asset touch up

--- DeltNU-iOS/SceneDelegate.swift
+++ DeltNU-iOS/SceneDelegate.swift
@@ -23,7 +23,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: SessionView())
-            //.environmentObject(self.session)
             self.window = window
             window.makeKeyAndVisible()
         }

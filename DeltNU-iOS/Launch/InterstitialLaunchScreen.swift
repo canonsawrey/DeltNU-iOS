5d756504fd1ae905bf42fdbede8f648135b6022d
Canon Sawrey <csawrey@rightpoint.com>
2019-12-27 14:15:02 -0500

    A few changes

---
+++ DeltNU-iOS/Launch/InterstitialLaunchScreen.swift
@@ -0,0 +1,28 @@
+//
+//  InterstitialLaunchScreen.swift
+//  DeltNU
+//
+//  Created by Canon Sawrey on 12/27/19.
+//  Copyright © 2019 Canon Sawrey. All rights reserved.
+//
+
+import UIKit
+
+class InterstitialLaunchScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
